fe67bada8996ac03ad4272b66816f707c14ae4a5
Fabio Colognesi <fabio.colognesi@omniasolutions.eu>
2012-05-29 16:03:41 +0200

    Release on Version 6.0.3

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Product Lifecycle Management',
-    'version': '1.0',
+    'version': '3.0',
     'category': 'Generic Modules/Others',
     'depends': ['base', 'process','document','product','mrp'],
     'author': 'OmniaSolutions',
