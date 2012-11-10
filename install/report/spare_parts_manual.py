1a8cec362e9695bff9bfce21570c0417a3660ace
Fabio Colognesi <fabio.colognesi@omniasolutions.eu>
2012-11-10 16:03:26 +0100

    Changed rml location file to avoid to be tied to official addons path. Introduced functions to manage Bom sorting based on item position. Added bom_structure_one_cum_custom_report class, copied from bom_structure package to ensure Bom body paging in right way.

--- install/report/spare_parts_manual.py
+++ install/report/spare_parts_manual.py
@@ -33,6 +33,8 @@ from reportlab.lib.pagesizes import A4,cm
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle,Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 from report import report_sxw
+from operator import itemgetter
+
 # NOTE : TO BE ADDED TO FINAL CONFIGURATION. NOT IN STANDARD PYTHON
 from pyPdf import PdfFileWriter, PdfFileReader
 # NOTE : TO BE ADDED TO FINAL CONFIGURATION. NOT IN STANDARD PYTHON
@@ -100,19 +102,28 @@ def getBomRow(objRel):
          ]
     return ret
 
-#res['name']=l.name
-#res['item']=l.itemnum
-#res['pfather']=l.bom_id.product_id.name
-#res['pname']=l.product_id.name
-#res['pdesc']=l.product_id.description
-#res['pcode']=l.product_id.default_code
-#res['pqty']=l.product_qty
-#res['uname']=l.product_uom.name
-#res['pweight']=l.product_id.weight_net
-#res['code']=l.code
-#res['level']=level  
-  
+def _moduleName():
+    path = os.path.dirname(__file__)
+    return os.path.basename(os.path.dirname(os.path.dirname(path)))
+openerpModule=_moduleName()
 
+def _modulePath():
+    return os.path.dirname(__file__)
+openerpModulePath=_modulePath()
+
+
+def BomSort(object):
+    bomobject=[]
+    res={}
+    index=0
+    for l in object:
+        res[str(index)]=l.itemnum
+        index+=1
+    items = res.items()
+    items.sort(key = itemgetter(1))
+    for res in items:
+        bomobject.append(object[int(res[0])])
+    return bomobject
 
 
 class external_pdf(render):
@@ -124,13 +135,64 @@ class external_pdf(render):
 
     def _render(self):
         return self.pdf
-    
-HEDER=report_sxw.report_sxw("report.spare.parts.heder", 
+
+header_file=os.path.join(openerpModulePath,"spare_parts_header.rml")
+body_file=os.path.join(openerpModulePath,"spare_parts_body.rml")
+
+
+class bom_structure_one_sum_custom_report(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(bom_structure_one_sum_custom_report, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+            'get_children':self.get_children,
+        })
+
+    def get_children(self, object, level=0):
+        result=[]
+
+        def _get_rec(bomobject,level):
+            object=BomSort(bomobject)
+            tmp_result=[]
+            listed={}
+            keyIndex=0
+            for l in object:
+                res={}
+                if l.name in listed.keys():
+                    res=tmp_result[listed[l.name]]
+                    res['pqty']=res['pqty']+l.product_qty
+                    tmp_result[listed[l.name]]=res
+                else:
+                    res['name']=l.name
+                    res['item']=l.itemnum
+                    res['pname']=l.product_id.name
+                    res['pdesc']=l.product_id.description
+                    res['pcode']=l.product_id.default_code
+                    res['previ']=l.product_id.engineering_revision
+                    res['pqty']=l.product_qty
+                    res['uname']=l.product_uom.name
+                    res['pweight']=l.product_id.weight_net
+                    res['code']=l.code
+                    res['level']=level
+                    tmp_result.append(res)
+                    listed[l.name]=keyIndex
+                    keyIndex+=1
+            return result.extend(tmp_result)
+
+        children=_get_rec(object,level+1)
+
+        return result
+
+HEADER=report_sxw.report_sxw("report.spare.parts.header", 
                             "product.product", 
-                            rml='addons/OpenErpPlm/install/report/spare_parts_header.rml')   
+                            rml=header_file,
+                            header='internal')
+   
 BODY=report_sxw.report_sxw("report.spare.parts.body", 
                            "mrp.bom", 
-                           rml='addons/OpenErpPlm/install/report/spare_parts_header_body.rml')
+                           rml=body_file,
+                           parser=bom_structure_one_sum_custom_report,
+                           header='internal')
 
 class component_spare_parts_report(report_int):
     """
@@ -140,6 +202,7 @@ class component_spare_parts_report(report_int):
     def create(self, cr, uid, ids, datas, context=None):
         self.pool = pooler.get_pool(cr.dbname)
         componentType=self.pool.get('product.product')
+        bomType=self.pool.get('mrp.bom')
         userType=self.pool.get('res.users')
         user=userType.browse(cr, uid, uid, context=context)
         msg = "Printed by "+str(user.name)+" : "+ str(time.strftime("%d/%m/%Y %H:%M:%S"))
@@ -148,7 +211,7 @@ class component_spare_parts_report(report_int):
         for component in components:
             buf=self.getFirstPage(cr, uid, [component.id],context)
             output.addPage(buf)
-            self.getSparePartsPdfFile(cr, uid,context,component, output,componentType)
+            self.getSparePartsPdfFile(cr, uid, context, component, output, componentType, bomType)
         if output != None:
             pdf_string = StringIO.StringIO()
             output.collector.write(pdf_string)
@@ -158,26 +221,32 @@ class component_spare_parts_report(report_int):
             return (self.obj.pdf, 'pdf')
         return (False, '')    
    
-    def getSparePartsPdfFile(self,cr,uid,context,component,output,componentTemplate):
-        for pageStream in self.getPdfComponentLayout(component):
-            output.addPage(pageStream)
-        childrenids=componentTemplate._getChildrenBom(cr, uid, component, FIRST_LEVEL, context=context)
-        if component.id in childrenids:
-            childrenids.remove(component.id)
-        children=componentTemplate.browse(cr, uid, childrenids, context=context)
-        if len(children)>0:
-            baseBom=self.pool.get('mrp.bom')
-            childBomObject=baseBom.search(cr,uid,[('product_id','=',component.id),('type','=','spbom')])
-            if len(childBomObject)>0:
-                stream,type=BODY.create(cr, uid, childBomObject, data={'report_type': u'pdf'},context=context) 
-                pageStream=StringIO.StringIO()
-                pageStream.write(stream)
-                output.addPage(pageStream)
-                processed=[]
-                for child in children:
-                    if not child.id in processed:
-                        self.getSparePartsPdfFile(cr,uid,context,child,output,componentTemplate)   
-                processed.append(child.id) 
+    def getSparePartsPdfFile(self, cr, uid, context, component, output, componentTemplate, bomTemplate):
+        packedObjs=[]
+        packedIds=[]
+        bomIds=bomTemplate.search(cr,uid,[('product_id','=',component.id),('type','=','spbom')])
+#        if len(bomIds)<1:
+#            bomIds=bomTemplate.search(cr,uid,[('product_id','=',component.id),('type','=','normal')])
+#        if len(bomIds)<1:
+#            bomIds=bomTemplate.search(cr,uid,[('product_id','=',component.id),('type','=','ebom')])
+        if len(bomIds)>0:
+            BomObject=bomTemplate.browse(cr, uid, bomIds[0], context=context)
+            if BomObject:
+                for bom_line in BomObject.bom_lines:
+                    packedObjs.append(bom_line.product_id)
+                    packedIds.append(bom_line.id)
+                if len(packedIds)>0:
+                    for pageStream in self.getPdfComponentLayout(component):
+                        output.addPage(pageStream)
+                    stream,typerep=BODY.create(cr, uid, [BomObject.id], data={'report_type': u'pdf'},context=context) 
+                    pageStream=StringIO.StringIO()
+                    pageStream.write(stream)
+                    output.addPage(pageStream)
+                    processed=[]
+                    for packedObj in packedObjs:
+                        if not packedObj.id in processed:
+                            self.getSparePartsPdfFile(cr,uid,context,packedObj,output,componentTemplate,bomTemplate)   
+                    processed.append(packedObj.id) 
 
     def getPdfComponentLayout(self,component):
         ret=[]
@@ -189,7 +258,7 @@ class component_spare_parts_report(report_int):
     
     def getFirstPage(self,cr, uid, ids,context):
         strbuffer = StringIO.StringIO()
-        reportStream,reportType=HEDER.create(cr, uid, ids, data={'report_type': u'pdf'},context=context)
+        reportStream,reportType=HEADER.create(cr, uid, ids, data={'report_type': u'pdf'},context=context)
         strbuffer.write(reportStream)
         return strbuffer
           
