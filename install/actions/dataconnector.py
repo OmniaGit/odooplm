b9e5551a044747afe6deb6e3bb58389a296cda4d
Fabio Colognesi <fabio.colognesi@omniasolutions.eu>
2014-04-05 09:27:22 +0200

    Added to configuration, respecting v. 7 reorder.

---
+++ install/actions/dataconnector.py
@@ -0,0 +1,316 @@
+
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OmniaSolutions, Your own solutions
+#    Copyright (C) 2010 OmniaSolutions (<http://omniasolutions.eu>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import os
+import types
+import cPickle as pickle
+from datetime import datetime
+
+import logging
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+
+def normalize(value):
+    return unicode(str(value).replace('"','\"').replace("'",'\"').replace("%","%%").strip(), 'Latin1')
+
+class plm_temporary(osv.osv_memory):
+    _inherit = "plm.temporary"
+##  Specialized Actions callable interactively
+    def action_transferData(self, cr, uid, ids, context=None):
+        """
+            Call for Transfer Data method
+        """
+        if not 'active_id' in context:
+            return False
+        self.pool.get('product.product').TransferData(cr, uid)
+        return False
+#         return {
+#               'name': _('Products'),
+#               'view_type': 'form',
+#               "view_mode": 'tree,form',
+#               'res_model': 'product.product',
+#               'type': 'ir.actions.act_window',
+#               'domain': "[]",
+#          }
+    
+plm_temporary()
+
+
+class plm_component(osv.osv):
+    _inherit = 'product.product'
+
+###################################################################
+#   Override these properties to configure TransferData process.  #
+###################################################################
+
+    @property
+    def get_part_data_transfer(self):
+        """
+            Map OpenErp vs. destination Part data transfer fields
+        """
+        return {
+                'table' : 'PARTS',                          # Used with 'db' transfer
+                'status': ['released',],
+                'fields':
+                    {
+                    'name'                  : 'revname',
+                    'engineering_revision'  : 'revprog',
+                    'description'           : 'revdes',
+                    }
+                }
+
+    @property
+    def get_bom_data_transfer(self):
+        """
+            Map OpenErp vs. destination BoM data transfer fields
+        """
+        return {
+                'kind' : 'normal',                         # Bom type name to export
+                'table': 'BOMS',                           # Destination table name. Used with 'db' transfer
+                'PName': 'parent',                         # Parent column name.     Used with 'db' transfer
+                'CName': 'child',                          # Child column name.      Used with 'db' transfer
+                'fields':
+                    {
+                    'itemnum'               : 'relpos',
+                    'product_qty'           : 'relqty',
+                    }
+                }
+
+    @property
+    def get_data_transfer(self):
+        """
+            Map OpenErp vs. destination Connection DB data transfer values
+        """
+        return {
+                'db':
+                    {
+                    'protocol'              : 'mssql+pymssql',
+                    'user'                  : 'dbamkro',
+                    'password'              : 'dbamkro',
+                    'host'                  : 'SQL2K8\SQLEXPRESS',
+                    'database'              : 'Makro',
+                    },
+                
+                'file':
+                    {
+                    'exte'                  : 'csv',
+                    'separator'             : ',',
+                    'name'                  : 'transfer',
+                    'bomname'               : 'transferbom',
+                    'directory'             : '/tmp',
+                    }
+                }
+
+###################################################################
+#   Override these properties to configure TransferData process.  #
+###################################################################
+
+    @property
+    def get_last_session(self):
+        """
+            Get last execution date & time as stored.
+                format => '%Y-%m-%d %H:%M:%S'
+        """
+        lastDate=datetime.now()
+        fileName=os.path.join(os.environ.get('HOME'),'ttsession.time')
+        if os.path.exists(fileName):
+            try:
+                fobj=open(fileName)
+                lastDate=pickle.load(fobj)
+                fobj.close()
+            except:
+                try:
+                    fobj.close()
+                except:
+                    pass
+                os.unlink(fileName)
+        else:
+            self.set_last_session
+        return lastDate.strftime('%Y-%m-%d %H:%M:%S')
+
+    @property
+    def set_last_session(self):
+        """
+            Get last execution date & time as stored.
+                format => '%Y-%m-%d %H:%M:%S'
+        """
+        lastDate=datetime.now()
+        fileName=os.path.join(os.environ.get('HOME'),'ttsession.time')
+        if os.path.exists(fileName):
+            os.unlink(fileName)
+        fobj=open(fileName,'w')
+        pickle.dump(lastDate,fobj)
+        fobj.close()
+        return lastDate.strftime('%Y-%m-%d %H:%M:%S')
+
+    def TransferData(self, cr, uid, ids=False, context=None):
+ 
+        operation=False
+        reportStatus='Failed'
+        updateDate=self.get_last_session
+        logging.debug("[TransferData] Start : %s" %(str(updateDate)))
+        transfer=self.get_data_transfer
+        datamap=self.get_part_data_transfer['fields']
+        datatyp=self.get_part_data_transfer['types']
+        fieldsListed=datamap.keys()
+        allIDs=self.query_data(cr, uid, updateDate, self.get_part_data_transfer['status'])
+        tmpData=self.export_data(cr, uid, allIDs, fieldsListed)
+        if tmpData.get('datas'):
+            if 'db' in transfer:
+                import dbconnector
+                dataTargetTable=self.get_part_data_transfer['table']
+                connection=dbconnector.get_connection(transfer['db'])
+            
+                checked=dbconnector.saveParts(self,cr, uid, connection, tmpData.get('datas'), dataTargetTable, datamap,datatyp)
+    
+                if checked:
+                    bomTargetTable=self.get_bom_data_transfer['table']
+                    bomdatamap=self.get_bom_data_transfer['fields']
+                    bomdatatyp=self.get_bom_data_transfer['types']
+                    parentName=self.get_bom_data_transfer['PName']
+                    childName=self.get_bom_data_transfer['CName']
+                    kindBomname=self.get_bom_data_transfer['kind']
+                    operation=dbconnector.saveBoms(self, cr, uid, connection, checked, allIDs, dataTargetTable, datamap, datatyp, kindBomname, bomTargetTable, parentName, childName, bomdatamap, bomdatatyp)  
+                     
+            if 'file' in transfer:
+                bomfieldsListed=self.get_bom_data_transfer['fields'].keys()
+                kindBomname=self.get_bom_data_transfer['kind']
+                operation=self.extract_data(cr, uid, allIDs, kindBomname, fieldsListed, bomfieldsListed, transfer['file'])
+
+        if operation:
+            updateDate=self.set_last_session
+            reportStatus='Successful'
+            
+        logging.debug("[TransferData] %s End : %s" %(reportStatus,str(updateDate)))
+        return False
+
+    def query_data(self, cr, uid, updateDate, statuses=[]):
+        """
+            Query to return values based on columns selected.
+                updateDate => '%Y-%m-%d %H:%M:%S'
+        """
+        if not statuses:
+            statusList=['released']
+        else:
+            statusList=statuses
+            
+        allIDs=self.search(cr,uid,[('write_date','>',updateDate),('state','in',statusList)],order='engineering_revision')
+        allIDs.extend(self.search(cr,uid,[('create_date','>',updateDate),('state','in',statusList)],order='engineering_revision'))
+        return list(set(allIDs))
+
+    def extract_data(self,cr,uid,allIDs, kindBomname='normal', anag_fields=False, rel_fields=False, transferdata={}):
+        """
+            action to be executed for Transmitted state.
+            Transmit the object to ERP Metodo
+        """
+        
+        def getChildrenBom(component, kindName):
+            for bomid in component.bom_ids:
+                if not (str(bomid.type).lower()==kindName):
+                    continue
+                return bomid.bom_lines
+            return []
+        
+        if not anag_fields:
+            anag_fields=['name','description']
+        if not rel_fields:
+            rel_fields=['bom_id','product_id','product_qty','itemnum']
+
+        if not transferdata:
+            outputpath=os.environ.get('TEMP')
+            tmppws=os.environ.get('OPENPLMOUTPUTPATH')
+            if tmppws!=None and os.path.exists(tmppws):
+                outputpath=tmppws
+            exte='csv'
+            fname=datetime.now().isoformat(' ').replace('.','').replace(':','').replace(' ','').replace('-','')+'.'+exte
+            bomname="bom"
+        else:
+            outputpath=transferdata['directory']
+            exte="%s" %(str(transferdata['exte']))
+            fname="%s.%s" %(str(transferdata['name']),exte)
+            bomname="%s" %(str(transferdata['bomname']))
+            
+        if outputpath==None:
+            return True
+        if not os.path.exists(outputpath):
+            raise osv.except_osv(_('Export Data Error'), _("Requested writing path (%s) doesn't exist." %(outputpath)))
+            return False 
+
+        filename=os.path.join(outputpath,fname)
+        expData=self.export_data(cr, uid, allIDs,anag_fields)
+        if not self.export_csv(filename, anag_fields, expData, True):
+            raise osv.except_osv(_('Export Data Error'), _("Writing operations on file (%s) have failed." %(filename)))
+            return False
+        
+        ext_fields=['parent','child']
+        ext_fields.extend(rel_fields)
+        for oic in self.browse(cr, uid, allIDs, context=None):
+            dataSet=[]
+            fname="%s-%s.%s" %(bomname,str(oic.name),exte)
+            filename=os.path.join(outputpath,fname)
+            for oirel in getChildrenBom(oic, kindBomname):
+                rowData=[oic.name,oirel.product_id.name]
+                for rel_field in rel_fields:
+                    rowData.append(eval('oirel.%s' %(rel_field)))
+                dataSet.append(rowData)
+            if dataSet:
+                expData={'datas': dataSet}
+                
+                if not self.export_csv(filename, ext_fields, expData, True):
+                    raise osv.except_osv(_('Export Data Error'), _("No Bom extraction files was generated, about entity (%s)." %(fname)))
+                    return False
+        return True
+
+    def export_csv(self, fname, fields=[], result={}, write_title=False):
+        import csv, stat
+        if not ('datas' in result) or not result:
+            logging.error("export_csv : No 'datas' in result.")
+            return False
+
+        if not fields:
+            logging.error("export_csv : No 'fields' in result.")
+            return False
+        
+        try:
+            fp = file(fname, 'wb+')
+            writer = csv.writer(fp)
+            if write_title:
+                writer.writerow(fields)
+            results=result['datas']
+            for datas in results:
+                row = []
+                for data in datas:
+                    if type(data)==types.StringType:
+                        row.append(data.replace('\n',' ').replace('\t',' '))
+                    else:
+                        row.append(data or '')
+                writer.writerow(row)
+            fp.close()
+            os.chmod(fname, stat.S_IRWXU|stat.S_IRWXO|stat.S_IRWXG)
+            return True
+        except IOError, (errno, strerror):
+            logging.error("export_csv : IOError : "+str(errno)+" ("+str(strerror)+").")
+            return False
+
+plm_component()
+
+
---
+++ install/actions/dbconnector.py
@@ -0,0 +1,192 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OmniaSolutions, Your own solutions
+#    Copyright (C) 2010 OmniaSolutions (<http://omniasolutions.eu>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import os
+import types
+from datetime import datetime
+from sqlalchemy import *
+import logging
+
+from openerp.osv import osv, fields
+
+def normalize(value):
+    return unicode(str(value).replace('"','\"').replace("'",'\"').replace("%","%%").strip(), 'Latin1')
+
+
+def get_connection(dataConn):
+    """
+        Get last execution date & time as stored.
+            format => '%Y-%m-%d %H:%M:%S'
+    """
+    connection=False
+    try:
+        connectionString=r'%s://%s:%s@%s/%s' %(dataConn['protocol'],dataConn['user'],dataConn['password'],dataConn['host'],dataConn['database'])
+        engine = create_engine(connectionString, echo=False)
+        connection = engine.connect()
+    except Exception,ex:
+        logging.error("[get_connection] : Error to connect (%s)." %(str(ex)))
+    return connection
+
+def saveParts(ObjectOE, cr, uid, connection, prtInfos, targetTable, datamap, datatyp):
+    """
+        Updates parts if exist in DB otherwise it create them.
+    """
+    checked={}
+    if connection:
+        trans = connection.begin()
+        for prtInfo in prtInfos:
+            prtDict=dict(zip(datamap.keys(),prtInfo))
+
+            if 'name' in prtDict:
+                prtName=prtDict['name']
+            else:
+                continue
+
+            string1="delete from %s where %s='%s'" %(targetTable,datamap['name'],normalize(prtName))
+            connection.execution_options(autocommit=False).execute(string1)
+                             
+            separator=""
+            namesString=""
+            valuesString=""
+            for column in prtDict.keys():
+                if (datatyp[column] == 'datetime'):
+                    if prtDict[column]:
+                        namesString+="%s %s" %(separator,datamap[column])
+                        valuesString+="%s '%s'" %(separator,datetime.strptime(prtDict[column],"%Y-%m-%d %H:%M:%S"))
+                elif (datatyp[column] == 'int'):
+                    namesString+="%s %s" %(separator,datamap[column])
+                    valuesString+="%s %d" %(separator,prtDict[column])
+                elif (datatyp[column] == 'bool'):
+                    namesString+="%s %s" %(separator,datamap[column])
+                    valuesString+="%s %d" %(separator,prtDict[column])
+                elif (datatyp[column] =='float'):
+                    namesString+="%s %s" %(separator,datamap[column])
+                    valuesString+="%s %f" %(separator,prtDict[column])
+                elif (datatyp[column] == 'char'):
+                    namesString+="%s %s" %(separator,datamap[column])
+                    valuesString+="%s '%s'" %(separator,normalize(prtDict[column]).replace("False",''))
+                if len(namesString)>0:
+                    separator=","
+
+            try:
+                string1="insert into %s (%s) values (%s)" %(targetTable,namesString,valuesString)
+                connection.execution_options(autocommit=False).execute(string1)
+                checked[prtName]=prtDict
+            except Exception,ex:
+                checked[prtName]=False
+        trans.commit()
+    return checked
+
+def saveBoms(ObjectOE, cr, uid, connection, checked, allIDs, dataTargetTable, datamap, datatyp, kindBomname, bomTargetTable, parentColName, childColName, bomdatamap, bomdatatyp):
+
+    
+    def checkChildren(ObjectOE, cr, uid, connection, components, datamap):
+        for component in components:
+            for bomid in component.bom_ids:
+                if not (str(bomid.type).lower()==kindName):
+                    continue
+                for bom in bomid.bom_lines:
+                    if not bom.product_id.name in childNames:
+                        childNames.append(bom.product_id.name)
+                        childIDs.append(bom.product_id.id)
+                        
+        tmpData=ObjectOE.export_data(cr, uid, childIDs, datamap.keys())
+        return saveParts(ObjectOE, cr, uid, connection, tmpData.get('datas'), dataTargetTable, datamap, datatyp)
+
+    def removeBoms(connection, bomTargetTable, parentColName, parentName):
+        trans = connection.begin()
+        try:
+            string1="delete from %s where %s = '%s'" %(bomTargetTable,parentColName,parentName)
+            connection.execution_options(autocommit=False).execute(string1)
+        except Exception,ex:
+            logging.error("[saveBoms::removeBoms] : Exception (%s) cleaning bom (%s)." %(str(ex),parentName))
+        trans.commit()
+
+    kindName=kindBomname.lower()
+    relation=False
+    childNames=checked.keys()
+    childIDs=[]
+    entityChecked=checked
+    
+    if connection:
+        trans = connection.begin()
+        components=ObjectOE.browse(cr, uid, allIDs)
+        entityChecked.update(checkChildren(ObjectOE, cr, uid, connection, components, datamap))
+                         
+        for component in components:
+            if not component.name in entityChecked.keys():
+                logging.error("[saveBoms] : Product (%s) is not in current data package." %(component.name))
+                continue
+            entityFather=entityChecked[component.name]
+            if not entityFather:
+                logging.error("[saveBoms] : Product (%s), as father, seems it could be not saved." %(component.name))
+                continue
+            
+            for bomid in component.bom_ids:
+                if not (str(bomid.type).lower()==kindName):
+                    continue
+                
+                removeBoms(connection, bomTargetTable, parentColName, component.name)
+
+                for bom in bomid.bom_lines:
+                    if not bom.product_id.name in entityChecked.keys():
+                        logging.error("[saveBoms] : Product (%s) is not in current data package." %(bom.product_id.name))
+                        continue
+
+                    entityChild=entityChecked[bom.product_id.name]
+                    if not entityChild:
+                        logging.error("[saveBoms] : Product (%s), as child, seems it could be not saved." %(bom.product_id.name))
+                        continue
+
+                    separator=","
+                    namesString="%s, %s" %(parentColName, childColName)
+                    valuesString="'%s', '%s'" %(normalize(component.name),normalize(bom.product_id.name))
+                    
+                    expData=ObjectOE.pool.get('mrp.bom').export_data(cr, uid, [bom.id], bomdatamap.keys())
+                    if expData.get('datas'):
+                        bomDict=dict(zip(bomdatamap.keys(),expData.get('datas')[0]))                                       
+                        for column in bomDict:
+                            if (bomdatatyp[column] == 'datetime'):
+                                if bomDict[column]:
+                                    namesString+="%s %s" %(separator,bomdatamap[column])
+                                    valuesString+="%s '%s'" %(separator,datetime.strptime(bomDict[column],"%Y-%m-%d %H:%M:%S"))
+                            elif (bomdatatyp[column] == 'int'):
+                                namesString+="%s %s" %(separator,bomdatamap[column])
+                                valuesString+="%s %d" %(separator,int(bomDict[column]))
+                            elif (bomdatatyp[column] == 'bool'):
+                                namesString+="%s %s" %(separator,bomdatamap[column])
+                                valuesString+="%s %d" %(separator,bomDict[column])
+                            elif (bomdatatyp[column] =='float'):
+                                namesString+="%s %s" %(separator,bomdatamap[column])
+                                valuesString+="%s %f" %(separator,float(bomDict[column]))
+                            elif (bomdatatyp[column] == 'char'):
+                                namesString+="%s %s" %(separator,bomdatamap[column])
+                                valuesString+="%s '%s'" %(separator,normalize(bomDict[column]).replace("False",''))
+
+                        try:
+                            string1="insert into %s (%s) values (%s)" %(bomTargetTable, namesString,valuesString)
+                            connection.execution_options(autocommit=False).execute(string1)
+                        except Exception:
+                            logging.error("[saveBoms] : Parent (%s) Child (%s), relation not saved." %(component.name,bom.product_id.name))
+        trans.commit()
+        return True
+
+    return False
