00fcaf76b3dd7bf090147b7c50b7ae25af49826c
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2016-07-20 12:29:28 +0100

    FIX:	- Report obsoleted fixed

--- plm_date_bom/extended_class/bom_obsoleted_report.py
+++ plm_date_bom/extended_class/bom_obsoleted_report.py
@@ -29,92 +29,11 @@ from openerp.osv import osv
 from openerp.report import report_sxw
 from openerp import _
 import time
-
-
-def _translate(value):
-    return _(value)
-
-
-def get_bom_report(myObject, recursion=False, flat=False, leaf=False, level=1, summarize=False):
-
-    def summarize_level(bomObj, recursion=False, flat=False, level=1, leaf=False, parentKey='', summarize=False):
-        if leaf:
-            recursion = True
-        for l in bomObj.bom_line_ids:
-            res = {}
-            product = l.product_id.product_tmpl_id
-            myKey = product.name
-            if recursion or leaf or flat:
-                myNewBom = None
-                for bomBws in l.product_id.bom_ids:
-                    if bomBws.type == l.bom_id.type:
-                        myNewBom = bomBws
-                        break
-                if leaf and not myNewBom:
-                    myKey = 'leaf_' + myKey
-                elif not flat:
-                    myKey = parentKey + myKey + '_' + str(level)
-                if myNewBom:
-                    if flat or myKey not in listed.keys():
-                        summarize_level(myNewBom, recursion, flat, level + 1, leaf, myKey, summarize)
-            if myKey in listed.keys() and summarize:
-                listed[myKey]['pqty'] = listed[myKey].get('pqty', 0) + l.product_qty
-            else:
-                res['name'] = product.engineering_code
-                res['item'] = l.itemnum
-                res['pname'] = product.engineering_code
-                res['pdesc'] = _(product.description)
-                res['pcode'] = l.product_id.default_code
-                res['previ'] = product.engineering_revision
-                res['pqty'] = l.product_qty
-                res['uname'] = l.product_uom.name
-                res['pweight'] = product.weight
-                res['code'] = l.product_id.default_code
-                res['level'] = level
-                res['prodBrws'] = l.product_id
-                res['prodTmplBrws'] = product
-                if leaf and 'leaf_' not in myKey:
-                    continue
-                if product.state == 'obsoleted':
-                    listed[myKey] = res
-                    LL = sortIndex.setdefault(res['item'], [])
-                    LL.append(myKey)
-                    sortIndex[res['item']] = LL
-
-    listed = {}
-    sortIndex = {}
-    summarize_level(myObject, recursion, flat, level, leaf, '', summarize)
-    kkk = sortIndex.keys()
-    kkk.sort()
-    out = []
-    for i in kkk:
-        for pName in sortIndex.get(i, []):
-            out.append(listed.get(pName, {}))
-    return out
-
-
-class bom_structure_obsoleted(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context):
-        super(bom_structure_obsoleted, self).__init__(cr, uid, name, context=context)
-        self.keyIndex = 0
-        self.localcontext.update({
-            'time': time,
-            'get_children': self.get_children,
-            'bom_type': self.bom_type,
-            'trans': _translate,
-            'context': context,
-        })
-
-    def get_children(self, myObject, level=1):
-        return get_bom_report(myObject, recursion=True, flat=False, leaf=False, level=1, summarize=False)
-
-    def bom_type(self, myObject):
-        result = dict(self.pool.get(myObject._model._name).fields_get(self.cr, self.uid)['type']['selection']).get(myObject.type, '')
-        return _(result)
+from openerp.addons.plm.install.report.bom_structure import bom_structure_all_custom_report
 
 
 class report_plm_bom_obsoleted(osv.AbstractModel):
     _name = 'report.plm_date_bom.plm_bom_obsoleted'
     _inherit = 'report.abstract_report'
     _template = 'plm_date_bom.plm_bom_obsoleted'
-    _wrapped_report_class = bom_structure_obsoleted
+    _wrapped_report_class = bom_structure_all_custom_report
