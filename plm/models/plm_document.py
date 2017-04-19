53368ae06763645da70d1dfc377fbe52cfaf42cc
dsmerghetto <daniel.smerghetto@omniasolutions.eu>
2017-04-19 14:44:36 +0200

    FIX:	- Bug on purge check-in documents

--- plm/models/plm_document.py
+++ plm/models/plm_document.py
@@ -754,7 +754,7 @@ class PlmDocument(models.Model):
                 if len(plmDocList) > 0:
                     ids = plmDocList.ids
                     ids.sort()
-                    res.append([fileName, not (self.browse(ids[len(ids) - 1]))])._is_checkedout_for_me()
+                    res.append([fileName, not self.browse(ids[len(ids) - 1])._is_checkedout_for_me()])
             return res
 
         if len(files) > 0:  # no files to process
