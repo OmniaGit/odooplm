a5f299276118b2171c0117dad3e193c0a173f23b
José L. S. A <alagunasalahaddin@live.com>
2018-09-17 17:08:47 +0300

    [plm-spare] :fire: beautify reports.
    Maybe they will require test.

--- plm_spare/report/spare_parts_manual.py
+++ plm_spare/report/spare_parts_manual.py
@@ -19,54 +19,48 @@
 #
 ##############################################################################
 
-'''
+"""
 Created on Apr 15, 2016
 
 @author: Daniel Smerghetto
-'''
+"""
 
-from odoo.osv import osv
-from operator import itemgetter
-from odoo import _
-import odoo
-from odoo import api
-from odoo import models
-from odoo.addons.plm.report.book_collector import BookCollector
-from odoo.addons.plm.report.book_collector import getBottomMessage
-
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-import time
 from io import BytesIO
 import base64
 import os
 import logging
 from datetime import datetime
-from dateutil import tz
+import time
 
+from operator import itemgetter
+from odoo import _
+import odoo
+from odoo import api
+from odoo import models
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
-from PyPDF2 import PdfFileWriter
-from PyPDF2 import PdfFileReader
+from odoo.addons.plm.report.book_collector import BookCollector
+from odoo.addons.plm.report.book_collector import getBottomMessage
 
-
-def isPdf(fileName):
-    if (os.path.splitext(fileName)[1].lower() == '.pdf'):
+def is_pdf(file_name):
+    if os.path.splitext(file_name)[1].lower() == '.pdf':
         return True
     return False
 
 
-def getDocumentStream(docRepository, objDoc):
+def get_document_stream(doc_repository, obj_doc):
     """
         Gets the stream of a file
     """
     content = False
     try:
-        if (not objDoc.store_fname) and (objDoc.db_datas):
-            content = base64.b64decode(objDoc.db_datas)
+        if (not obj_doc.store_fname) and (obj_doc.db_datas):
+            content = base64.b64decode(obj_doc.db_datas)
         else:
-            with open(os.path.join(docRepository, objDoc.store_fname), 'rb') as f:
+            with open(os.path.join(doc_repository, obj_doc.store_fname), 'rb') as f:
                 content = f.read()
     except Exception as ex:
-        logging.error("getFileStream : Exception (%s)reading  stream on file : %s." % (ex, objDoc.datas_fname))
+        logging.error("getFileStream : Exception ({0})reading  stream on file : {1}.".format(ex, obj_doc.datas_fname))
     return content
 
 
@@ -74,42 +68,42 @@ def _translate(value):
     return _(value)
 
 
-def BomSort(myObject):
-    bomobject = []
+def bom_sort(my_object):
+    bom_object = []
     res = {}
     index = 0
-    for l in myObject:
+    for l in my_object:
         res[str(index)] = l.itemnum
         index += 1
     items = res.items()
     items.sort(key=itemgetter(1))
     for res in items:
-        bomobject.append(myObject[int(res[0])])
-    return bomobject
+        bom_object.append(my_object[int(res[0])])
+    return bom_object
 
 
-def get_parent(myObject):
-    return [myObject.product_tmpl_id.name,
+def get_parent(my_object):
+    return [my_object.product_tmpl_id.name,
             '',
-            _(myObject.product_tmpl_id.name) or _(myObject.product_tmpl_id.default_code),
-            myObject.product_tmpl_id.engineering_revision,
-            _(myObject.product_tmpl_id.name),
+            _(my_object.product_tmpl_id.name) or _(my_object.product_tmpl_id.default_code),
+            my_object.product_tmpl_id.engineering_revision,
+            _(my_object.product_tmpl_id.name),
             '',
             '',
-            myObject.product_qty,
+            my_object.product_qty,
             '',
-            myObject.weight_net,
+            my_object.weight_net,
             ]
 
 
-class report_spare_parts_header(models.AbstractModel):
+class ReportSparePartsHeader(models.AbstractModel):
     _name = 'report.plm_spare.bom_spare_header'
 
-    def get_document_brws(self, objProduct):
+    def get_document_brws(self, obj_product):
         oldest_obj = None
         oldest_dt = None
-        if objProduct:
-            for linkedBrwsDoc in objProduct.linkeddocuments:
+        if obj_product:
+            for linkedBrwsDoc in obj_product.linkeddocuments:
                 create_date_str = linkedBrwsDoc.create_date
                 create_date = datetime.strptime(create_date_str, DEFAULT_SERVER_DATETIME_FORMAT)
                 if oldest_dt is None or create_date < oldest_dt:
@@ -117,8 +111,8 @@ class report_spare_parts_header(models.AbstractModel):
                     oldest_obj = linkedBrwsDoc
         return oldest_obj
 
-    def get_report_values(self, docids, data=None):
-        products = self.env['product.product'].browse(docids)
+    def get_report_values(self, doc_ids):
+        products = self.env['product.product'].browse(doc_ids)
         return {'docs': products,
                 'time': time,
                 'get_document_brws': self.get_document_brws}
@@ -135,81 +129,81 @@ class ReportSpareDocumentOne(models.AbstractModel):
         recursion = True
         if ReportSpareDocumentOne._name == 'report.plm_spare.pdf_one':
             recursion = False
-        self.processedObjs = []
+        self.processed_objs = []
 
-        componentType = self.env['product.product']
-        bomType = self.env['mrp.bom']
+        component_type = self.env['product.product']
+        bom_type = self.env['mrp.bom']
         user = self.env['res.users'].browse(self.env.uid)
         msg = getBottomMessage(user, self.env.context)
-        mainBookCollector = BookCollector(customTest=(True, msg))
+        main_book_collector = BookCollector(customTest=(True, msg))
         for component in components:
-            self.processedObjs = []
-            buf = self.getFirstPage([component.id])
-            mainBookCollector.addPage((buf, ''))
-            self.getSparePartsPdfFile(component, mainBookCollector, componentType, bomType, recursion)
-        if mainBookCollector is not None:
+            self.processed_objs = []
+            buf = self.get_first_page([component.id])
+            main_book_collector.addPage((buf, ''))
+            self.get_spare_parts_pdf_file(component, main_book_collector, component_type, bom_type, recursion)
+        if main_book_collector is not None:
             pdf_string = BytesIO()
-            mainBookCollector.collector.write(pdf_string)
+            main_book_collector.collector.write(pdf_string)
             out = pdf_string.getvalue()
             pdf_string.close()
-            byteString = b"data:application/pdf;base64," + base64.b64encode(out)
-            return byteString.decode('UTF-8')
+            byte_string = b"data:application/pdf;base64," + base64.b64encode(out)
+            return byte_string.decode('UTF-8')
         logging.warning('Unable to create PDF')
-        return (False, '')
+        return False, ''
 
-    def getSparePartsPdfFile(self, product, output, componentTemplate, bomTemplate, recursion):
-        packedObjs = []
-        packedIds = []
-        if product in self.processedObjs:
+    def get_spare_parts_pdf_file(self, product, output, component_template, bom_template, recursion):
+        packed_objs = []
+        packed_ids = []
+        if product in self.processed_objs:
             return
-        bomBrwsIds = bomTemplate.search([('product_id', '=', product.id), ('type', '=', 'spbom')])
-        if len(bomBrwsIds) < 1:
-            bomBrwsIds = bomTemplate.search([('product_tmpl_id', '=', product.product_tmpl_id.id), ('type', '=', 'spbom')])
-        if len(bomBrwsIds) > 0:
-            if bomBrwsIds:
-                self.processedObjs.append(product)
-                for bom_line in bomBrwsIds.bom_line_ids:
-                    packedObjs.append(bom_line.product_id)
-                    packedIds.append(bom_line.id)
-                if len(packedIds) > 0:
-                    for pageStream in self.getPdfComponentLayout(product):
+        bom_brws_ids = bom_template.search([('product_id', '=', product.id), ('type', '=', 'spbom')])
+        if len(bom_brws_ids) < 1:
+            bom_brws_ids = bom_template.search([('product_tmpl_id', '=', product.product_tmpl_id.id), ('type', '=', 'spbom')])
+        if len(bom_brws_ids) > 0:
+            if bom_brws_ids:
+                self.processed_objs.append(product)
+                for bom_line in bom_brws_ids.bom_line_ids:
+                    packed_objs.append(bom_line.product_id)
+                    packed_ids.append(bom_line.id)
+                if len(packed_ids) > 0:
+                    for page_stream in self.get_pdf_component_layout(product):
                         try:
-                            output.addPage((pageStream, ''))
+                            output.addPage((page_stream, ''))
                         except Exception as ex:
                             logging.error(ex)
                             raise ex
-                    pdf = self.env.ref('plm.report_plm_bom_structure_one').sudo().render_qweb_pdf(bomBrwsIds.ids)[0]
-                    pageStream = BytesIO()
-                    pageStream.write(pdf)
-                    output.addPage((pageStream, ''))
+                    pdf = self.env.ref('plm.report_plm_bom_structure_one').sudo().render_qweb_pdf(bom_brws_ids.ids)[0]
+                    page_stream = BytesIO()
+                    page_stream.write(pdf)
+                    output.addPage((page_stream, ''))
                     if recursion:
-                        for packedObj in packedObjs:
-                            if packedObj not in self.processedObjs:
-                                self.getSparePartsPdfFile(packedObj, output, componentTemplate, bomTemplate, recursion)
+                        for packed_obj in packed_objs:
+                            if packed_obj not in self.processed_objs:
+                                self.get_spare_parts_pdf_file(packed_obj, output, component_template, bom_template, recursion)
 
-    def getPdfComponentLayout(self, component):
+    def get_pdf_component_layout(self, component):
         ret = []
-        docRepository = self.env['plm.document']._get_filestore()
+        doc_repository = self.env['plm.document']._get_filestore()
         for document in component.linkeddocuments:
-            if (document.usedforspare) and (document.type == 'binary'):
+            if document.used_for_spare and (document.type == 'binary'):
                 if document.printout and str(document.printout) != 'None':
                     ret.append(BytesIO(base64.b64decode(document.printout)))
-                elif isPdf(document.datas_fname):
-                    value = getDocumentStream(docRepository, document)
+                elif is_pdf(document.datas_fname):
+                    value = get_document_stream(doc_repository, document)
                     if value:
                         ret.append(BytesIO(value))
         return ret
 
-    def getFirstPage(self, ids):
-        strbuffer = BytesIO()
+    def get_first_page(self, ids):
+        str_buffer = BytesIO()
         # todo: si rompe qui con v11 .. capire come fare il report da codice 
         pdf = self.env.ref('plm_spare.report_product_product_spare_header').sudo().render_qweb_pdf(ids)[0]
-        strbuffer.write(pdf)
-        return strbuffer
+        str_buffer.write(pdf)
+        return str_buffer
 
     @api.model
-    def get_report_values(self, docids, data=None):
-        documents = self.env['product.product'].browse(docids)
+    def get_report_values(self, doc_ids):
+        documents = self.env['product.product'].browse(doc_ids)
         return {'docs': documents,
                 'get_content': self.create}
 
