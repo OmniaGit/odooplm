71b6b2506654947221801bd581a4e48304843e85
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2019-03-12 17:59:53 +0100

    FIX:	- Check on plm_document / ir_attachment

--- plm/models/plm_document.py
+++ plm/models/plm_document.py
@@ -30,6 +30,7 @@ from odoo import SUPERUSER_ID
 from datetime import datetime
 import odoo.tools as tools
 from odoo.exceptions import UserError
+from odoo.exceptions import AccessError
 from odoo import models
 from odoo import fields
 from odoo import api
@@ -62,6 +63,58 @@ def create_directory(path):
     path = os.path.join(path, dir_name)
     os.makedirs(path)
     return dir_name
+
+class AttachmentExtension(models.Model):
+    _inherit = 'ir.attachment'
+
+    @api.model
+    def check(self, mode, values=None):
+        """Restricts the access to an ir.attachment, according to referred model
+        In the 'document' module, it is overriden to relax this hard rule, since
+        more complex ones apply there.
+        """
+        # collect the records to check (by model)
+        model_ids = defaultdict(set)            # {model_name: set(ids)}
+        require_employee = False
+        if self:
+            toExec = 'SELECT res_model, res_id, create_uid, public FROM %s WHERE id IN ' % (self._table)
+            toExec += '%s'
+            self._cr.execute(toExec, [tuple(self.ids)])
+            for res_model, res_id, create_uid, public in self._cr.fetchall():
+                if public and mode == 'read':
+                    continue
+                if not (res_model and res_id):
+                    if create_uid != self._uid:
+                        require_employee = True
+                    continue
+                model_ids[res_model].add(res_id)
+        if values and values.get('res_model') and values.get('res_id'):
+            model_ids[values['res_model']].add(values['res_id'])
+
+        # check access rights on the records
+        for res_model, res_ids in model_ids.iteritems():
+            # ignore attachments that are not attached to a resource anymore
+            # when checking access rights (resource was deleted but attachment
+            # was not)
+            if res_model not in self.env:
+                require_employee = True
+                continue
+            elif res_model == 'res.users' and len(res_ids) == 1 and self._uid == list(res_ids)[0]:
+                # by default a user cannot write on itself, despite the list of writeable fields
+                # e.g. in the case of a user inserting an image into his image signature
+                # we need to bypass this check which would needlessly throw us away
+                continue
+            records = self.env[res_model].browse(res_ids).exists()
+            if len(records) < len(res_ids):
+                require_employee = True
+            # For related models, check if we can write to the model, as unlinking
+            # and creating attachments can be seen as an update to the model
+            records.check_access_rights('write' if mode in ('create', 'unlink') else mode)
+            records.check_access_rule(mode)
+
+        if require_employee:
+            if not (self.env.user._is_admin() or self.env.user.has_group('base.group_user')):
+                raise AccessError(_("Sorry, you are not allowed to access this document."))
 
 
 class PlmDocument(models.Model):
