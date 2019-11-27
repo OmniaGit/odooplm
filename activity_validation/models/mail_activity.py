927456da34d80cff0c2d8430cdc72ba6d5ca1eac
Matteo Boscolo <matteo.oscolo@omniasolutions.eu>
2019-11-27 09:07:15 +0100

    Add  Activity_validation

---
+++ activity_validation/models/mail_activity.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OmniaSolutions, ERP-PLM-CAD Open Source Solutions
+#    Copyright (C) 2011-2019 https://OmniaSolutions.website
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this prograIf not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+'''
+Created on Nov 16, 2019
+
+@author: mboscolo
+'''
+import logging
+import datetime
+from odoo import models
+from odoo import fields
+from odoo import api
+from odoo import _
+from odoo.exceptions import UserError
+from datetime import timedelta
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+class MailActivity(models.Model):
+    _inherit = 'mail.activity'
+    
+    @api.multi
+    def action_done(self):
+        """ Wrapper without feedback because web button add context as
+        parameter, therefore setting context to feedback """
+        return super(MailActivity, self).action_done()
+    
+    @api.multi
+    def action_done_schedule_next(self):
+        """ Wrapper without feedback because web button add context as
+        parameter, therefore setting context to feedback """
+        return super(MailActivity, self).action_done_schedule_next()
+    
+    def unlink(self):
+        """
+        overload of the standard function in  order to do not delete the activity that are coming from activity validation
+        verifiy if activity is set t odone in the chatter
+        otherwise, create a storage for activity
+        """
+        return super(MailActivity, self).unlink()
