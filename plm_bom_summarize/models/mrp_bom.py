5c042ea42e34df9615b8f3a1e87e3af06cfab0b6
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2019-12-19 11:55:40 +0000

    IMP: add bom sommarization for client

---
+++ plm_bom_summarize/models/mrp_bom.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OmniaSolutions, ERP-PLM-CAD Open Source Solutions
+#    Copyright (C) 2011-2019 https://OmniaSolutions.website
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this prograIf not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+'''
+Created on 19 Dec 2019
+
+@author: mboscolo
+'''
+import logging
+import datetime
+from odoo import models
+from odoo import fields
+from odoo import api
+from odoo import _
+from odoo.exceptions import UserError
+from datetime import timedelta
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+class MrpBom(models.Model):
+    _inherit = 'mrp.bom'
+    
+    @api.model
+    def saveRelationNew(self,
+                        clientArgs):
+        new_context = self.env.context.copy()
+        new_context['SUMMARIZE_BOM'] = True
+        return super(MrpBom, self.with_context(new_context)).saveRelationNew(clientArgs)
+        
+        
+        
