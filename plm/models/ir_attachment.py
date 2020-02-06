2cc459d62cf39ee27445b53e9fda956104365f2f
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2020-02-06 15:12:25 +0100

    FIX:	- Purge PWS

--- plm/models/ir_attachment.py
+++ plm/models/ir_attachment.py
@@ -1077,7 +1077,7 @@ class PlmDocument(models.Model):
         def getcheckedfiles(files):
             res = []
             for fileName in files:
-                plmDocList = self.search([('datas_fname', '=', fileName)], order='revisionid')
+                plmDocList = self.search([('datas_fname', 'ilike', fileName)], order='revisionid DESC')
                 if len(plmDocList) > 0:
                     ids = plmDocList.ids
                     ids.sort()
