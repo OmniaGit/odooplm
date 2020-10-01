c3e3a077e06b8c2f57c36c2549c368084754d5e2
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2020-10-01 09:58:33 +0200

    FIX:	- Get last backup document (saving) independent from the user

--- plm/models/plm_backup_document.py
+++ plm/models/plm_backup_document.py
@@ -95,10 +95,9 @@ class PlmBackupDocument(models.Model):
             super(PlmBackupDocument, plm_backup_document_id).unlink()
 
     @api.model
-    def getLastBckDocumentByUser(self, doc_id, user_id):
+    def getLastBckDocumentByUser(self, doc_id):
         for obj in self.search([
-            ('documentid', '=', doc_id.id),
-            ('userid', '=', user_id.id)
+            ('documentid', '=', doc_id.id)
             ], order='create_date DESC', limit=1):
             return obj
         return self
