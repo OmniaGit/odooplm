4575ce55430ada02b89eba2384fbcebd6e5b4d0e
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2020-10-19 17:29:09 +0200

    FIX:	- Save checked in file

--- plm/models/ir_attachment.py
+++ plm/models/ir_attachment.py
@@ -2054,10 +2054,6 @@ class PlmDocument(models.Model):
         component_props, document_props, dbThread = clientArg[0]
         host_name = clientArg[1]
         host_pws = clientArg[2]
-        #  generate component
-        product_product_id = self.env['product.product'].createFromProps(component_props)
-        if not product_product_id:
-            logging.warning("Unable to create / get product_product from %s" % component_props)
         #  generate document
         ir_attachment_id, action = self.env['ir.attachment'].createFromProps(document_props,
                                                                              dbThread,
@@ -2065,6 +2061,10 @@ class PlmDocument(models.Model):
                                                                              host_pws)
         if not ir_attachment_id:
             logging.warning("Unable to create / get ir_attachment from %s" % document_props)
+        #  generate component
+        product_product_id = self.env['product.product'].createFromProps(component_props, ir_attachment_id)
+        if not product_product_id:
+            logging.warning("Unable to create / get product_product from %s" % component_props)
         #  generate link
         if product_product_id and ir_attachment_id:
             self.env['plm.component.document.rel'].createFromIds(product_product_id,
