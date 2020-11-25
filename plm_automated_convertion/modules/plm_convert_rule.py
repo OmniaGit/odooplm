fff24f7b7e04adee504fde8e268ac32060769c6e
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2020-11-25 16:24:59 +0100

    FIX:	- Updated rule

--- plm_automated_convertion/modules/plm_convert_rule.py
+++ plm_automated_convertion/modules/plm_convert_rule.py
@@ -35,5 +35,5 @@ class PlmConvertRule(models.Model):
     end_format = fields.Char('End Format')
     product_category = fields.Many2one('product.category', 'Category')
     server_id = fields.Many2one('plm.convert.servers', 'Conversion Server')
-    output_name_rule = fields.Char('Output Name Rule', default='{document.name}_{document.revisionid}')
+    output_name_rule = fields.Char('Output Name Rule', default="'%s_%s' % (document.name, document.revisionid)")
 
