b140d569a5be295b6b0701636ca166bfae7924ba
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2021-01-22 16:17:42 +0100

    FIX:	- Name search

--- plm/models/product_product.py
+++ plm/models/product_product.py
@@ -1716,10 +1716,9 @@ Please try to contact OmniaSolutions to solve this error, or install Plm Sale Fi
 
     @api.model
     def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
-        product_ids = self._search([('engineering_code', operator, name)] + args, limit=limit, access_rights_uid=name_get_uid)
-        ret = self.browse(product_ids).name_get()
-        ret += super(PlmComponent, self)._name_search(name, args, operator, limit, name_get_uid)
-        return list(set(ret))
+        product_ids = list(self._search([('engineering_code', operator, name)] + args, limit=limit, access_rights_uid=name_get_uid))
+        product_ids += list(super(PlmComponent, self)._name_search(name, args, operator, limit, name_get_uid))
+        return list(set(product_ids))
 
 
 class PlmTemporayMessage(osv.osv.osv_memory):
