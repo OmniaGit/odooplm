a5daf0d40eefa9a7a46aa46bc9b00e78fd3cc907
Leonardo Cazziolati <leonardo.cazziolati@omniasolutions.eu>
2021-03-03 15:12:48 +0100

    FIX: name_get in product_product

--- plm/models/product_product.py
+++ plm/models/product_product.py
@@ -1729,6 +1729,9 @@ Please try to contact OmniaSolutions to solve this error, or install Plm Sale Fi
             elif prod.engineering_code:
                 eng_code = '[%s] ' % ('%s_%s' % (prod.engineering_code, prod.engineering_revision))
             eng_code += prod.name
+            variant = prod.product_template_attribute_value_ids._get_combination_name()
+            if variant:
+                eng_code += ' (%s)' % variant
             result.append((prod.id, eng_code))
         return result
 
