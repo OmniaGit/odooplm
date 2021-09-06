048d771d2a5f130f6acaaa31097956edafffcd8e
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2021-09-06 11:47:02 +0200

    IMP:	- Added security

--- plm_project/models/mail_activity.py
+++ plm_project/models/mail_activity.py
@@ -24,14 +24,19 @@ Created on 30 Aug 2016
 
 @author: Daniel Smerghetto
 """
-from odoo import fields, models, _
-from datetime import datetime
+from odoo import models
+from odoo import _
+from odoo.exceptions import UserError
 
 
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
 
     def _action_done(self, feedback=False, attachment_ids=False):
+        for activity in self:
+            if self.checkProdConfirmedType(activity):
+                if self.checkSameUser(activity):
+                    raise UserError(_('You cannot mark as done a "Product Confirmed" activity if is not assigned to you.'))
         messages, activities = super(MailActivity, self)._action_done(feedback=feedback, attachment_ids=attachment_ids)
         for message in messages:
             if message.model == 'product.product':
@@ -39,3 +44,27 @@ class MailActivity(models.Model):
                 if product.state == 'confirmed':
                     product.action_release()
         return messages, activities
+
+    def checkProdConfirmedType(self, activity):
+        if activity.activity_type_id == self.env.ref('plm_project.mail_activity_product_confirmed'):
+            return True
+        return False
+
+    def checkSameUser(self, activity):
+        if self.env.user != activity.user_id or self.env.user.has_group('plm.group_plm_admin'):
+            return True
+        return False
+
+    def unlink(self):
+        for activity in self:
+            if self.checkProdConfirmedType(activity):
+                if self.checkSameUser(activity):
+                    raise UserError(_('You cannot unlink a "Product Confirmed" activity if is not assigned to you.'))
+        return super(MailActivity, self).unlink()
+
+    def write(self, vals):
+        for activity in self:
+            if self.checkProdConfirmedType(activity):
+                if self.checkSameUser(activity):
+                    raise UserError(_('You cannot edit a "Product Confirmed" activity if is not assigned to you.'))
+        return super(MailActivity, self).write(vals)
