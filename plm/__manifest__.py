d600bf1fbeaa79cbccf601ce54f40f9b061647f2
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2022-02-22 11:28:20 +0100

    IMP:  revision number

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "14.0.1.0.0",
+    "version": "14.0.2",
     "author": "OmniaSolutions",
     "website": "https://github.com/OmniaGit/odooplm",
     "category": "Manufacturing/Product Lifecycle Management",
