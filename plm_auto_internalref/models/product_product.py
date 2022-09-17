9e3fd144687c1ece5e56bd63623c07d29133cb41
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2022-09-17 07:24:33 +0200

    fix singleton error on multi editing on product

--- plm_auto_internalref/models/product_product.py
+++ plm_auto_internalref/models/product_product.py
@@ -48,7 +48,7 @@ class ProductProductExtension(models.Model):
     
     def computeDefaultCode(self,
                            vals={},
-                           objBrowse=None):
+                           product_product_ids=None):
         """
         Function to be overloaded for changing the inetrnal referense computation
         :vals dict like with all the value that be updated
@@ -59,13 +59,13 @@ class ProductProductExtension(models.Model):
         engineering_code = vals.get('engineering_code', '')
         engineering_revision = vals.get('engineering_revision', 0)
         default_code = vals.get('default_code')
-        if objBrowse: #suppose write operation
+        for product_product_id in product_product_ids: #suppose write operation
             if not engineering_code:
-                engineering_code = objBrowse.engineering_code
+                engineering_code = product_product_ids.engineering_code
             if not engineering_revision:
-                engineering_revision = objBrowse.engineering_revision
+                engineering_revision = product_product_ids.engineering_revision
             if not default_code:
-                default_code = objBrowse.default_code            
+                default_code = product_product_id.default_code            
         if in_revision and engineering_code and engineering_code != '-':
             out = self.getDefaultCodeTemplate % (engineering_code, engineering_revision)
         if engineering_code and not default_code and engineering_code != '-':
