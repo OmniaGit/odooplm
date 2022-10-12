659bb72dc938cdd7c95ac56f33489326efe97ca2
Daniel <omniadaniellav@gmail.com>
2022-10-12 15:52:45 +0200

    FIX:	- Multiple setup default code

--- plm_auto_internalref/models/product_product.py
+++ plm_auto_internalref/models/product_product.py
@@ -70,7 +70,7 @@ class ProductProductExtension(models.Model):
         if in_revision and engineering_code and engineering_code != '-':
             out = self.getDefaultCodeTemplate % (engineering_code, engineering_revision)
         if engineering_code and not default_code and engineering_code != '-':
-             out = self.getDefaultCodeTemplate % (engineering_code, engineering_revision)
+            out = self.getDefaultCodeTemplate % (engineering_code, engineering_revision)
         
         if default_code == out:
             return False
@@ -79,10 +79,13 @@ class ProductProductExtension(models.Model):
 
 
     def write(self, vals):
-        new_default_code = self.computeDefaultCode(vals,
-                                                   self)
-        if new_default_code :
-            logging.info('OdooPLM: Default Code set to %s ' % (new_default_code))
-            vals['default_code'] = new_default_code
-        return super(ProductProductExtension, self).write(vals)
+        ret = False
+        for product in self:
+            new_default_code = product.computeDefaultCode(vals,
+                                                          product)
+            if new_default_code:
+                logging.info('OdooPLM: Default Code set to %s ' % (new_default_code))
+                vals['default_code'] = new_default_code
+            ret = super(models.Model, product).write(vals)
+        return ret
 
