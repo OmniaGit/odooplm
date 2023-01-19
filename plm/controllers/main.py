d006bcf84d9c5ba917db29d8589badbe90d0d39d
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2023-01-19 09:23:58 +0100

    IMP: product camban view fix worning on views

--- plm/controllers/main.py
+++ plm/controllers/main.py
@@ -229,7 +229,7 @@ class UploadDocument(Controller):
                                                               'link_kind': 'ExtraTree'})    
             if product_id:
                 product_id = request.env['product.product'].browse(product_id)
-                request.env['plm.component.document.rel'].createFromIds(product_id, ir_attachment_id)           
+                #request.env['plm.component.document.rel'].createFromIds(product_id, ir_attachment_id)           
             return Response('Extra file Upload succeeded', status=200)
         logging.info('Extra file no upload %r' % (ir_attachment_id))
         return Response('Extra file Failed upload', status=400)
