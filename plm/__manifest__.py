ac39d3cccdc843e4287857d59d995d85420c7c3f
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2023-06-10 08:55:44 +0200

    imp: MANIFEST

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -26,7 +26,7 @@
     "category": "Manufacturing/Product Lifecycle Management",
     "sequence": 15,
     "license": "LGPL-3",
-    "summary": "PLM-PDM Integration with main CAD editors (SolidWorks, SolidEdge, Inventor, Autocad, Thinkdesign, Freecad)",
+    "summary": "PLM-PDM Integration with main CAD editors (SolidWorks, SolidEdge, Inventor, Autocad, DraftSight, Thinkdesign, Freecad)",
     "images": ["static/img/odoo_plm.png"],
     "depends": ["base", "board", "product", "mrp"],
     "data": [
