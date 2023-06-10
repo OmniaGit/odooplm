b4fa8b3c62d30f3f42059199e47f50b8272b78f5
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2023-06-10 08:54:39 +0200

    Merge branch '14.0' of https://github.com/OmniaGit/odooplm.git into 14.0

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "14.0.3",
+    "version": "14.0.4",
     "author": "OmniaSolutions",
     "website": "https://github.com/OmniaGit/odooplm",
     "category": "Manufacturing/Product Lifecycle Management",
