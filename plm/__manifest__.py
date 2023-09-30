6979cd8c861656df0481bd8d07634318d7549908
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2023-09-30 06:51:23 +0200

    FIX: permission on ir_attachment

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "16.0.15",
+    "version": "16.0.16",
     "author": "OmniaSolutions",
     "website": "https://odooplm.omniasolutions.website",
     "category": "Manufacturing/Product Lifecycle Management (PLM)",
