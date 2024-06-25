4fbac066be5582efc8617a411cb39a1b04d9f2e0
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-06-25 10:05:02 +0200

    ADD missini increment on manifest

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "14.0.5",
+    "version": "14.0.6",
     "author": "OmniaSolutions",
     "website": "https://github.com/OmniaGit/odooplm",
     "category": "Manufacturing/Product Lifecycle Management",
