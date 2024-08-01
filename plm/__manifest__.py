786a178b33b1bbab9029aaaaf7c465da80992f61
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-08-01 07:18:01 +0200

    TEST COMMIT

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "16.0.27",
+    "version": "16.0.28",
     "author": "OmniaSolutions",
     "website": "https://odooplm.omniasolutions.website",
     "category": "Manufacturing/Product Lifecycle Management (PLM)",
