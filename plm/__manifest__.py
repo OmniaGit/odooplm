8a1cfbf8566edb353dc6bbc8c256df4db06cbaae
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-10-12 09:04:21 +0200

    IMP: port fix from v16

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "17.0.0.7",
+    "version": "17.0.0.8",
     "author": "OmniaSolutions",
     "website": "https://odooplm.omniasolutions.website",
     "category": "Manufacturing/Product Lifecycle Management (PLM)",
