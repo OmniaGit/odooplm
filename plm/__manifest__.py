2d1e8856a55e6602fc1ea5a5fd62bdec600cdcca
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-10-15 11:29:54 +0200

    IMP: manifest

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "16.0.31",
+    "version": "16.0.32",
     "author": "OmniaSolutions",
     "website": "https://odooplm.omniasolutions.website",
     "category": "Manufacturing/Product Lifecycle Management (PLM)",
