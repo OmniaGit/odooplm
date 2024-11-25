1d82becdf94983abf0f5ee68f7953eee95a5fc9f
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-11-25 18:00:03 +0100

    add missing file

--- plm_sale_only_latest/models/product_template.py
+++ plm_sale_only_latest/models/product_template.py
@@ -23,29 +23,6 @@
 Created on 25 Aug 2016
 @author: Daniel Smerghetto
 """
-<<<<<<< HEAD
-import copy
-from odoo import api, models
-from odoo.addons.plm.models.plm_mixin import RELEASED_STATUSES
-
-
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-
-    @api.model
-    def name_search(self, name="", args=None, operator="ilike", limit=100):
-        conditional_status = copy.copy(RELEASED_STATUSES)
-        conditional_status.extend( self.env['ir.config_parameter'].sudo().get_param('sales_only_latest_params', '').split(','))
-        if self.env.context.get("sale_latest"):
-            args.insert(0,"&")
-            args.append(("|"))
-            args.append(("engineering_code", "=", False))
-            args.append(("engineering_state", "in", conditional_status))
-        return super(ProductTemplate, self).name_search(name=name, args=args, operator=operator, limit=limit)
-        
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-=======
 from odoo import api, models
 from odoo.addons.plm.models.plm_mixin import RELEASED_STATUSES
 
@@ -70,4 +47,3 @@ class ProductTemplate(models.Model):
         return super(ProductTemplate, self).name_search(
             name=name, args=args, operator=operator, limit=limit
         )
->>>>>>> branch '18.0' of https://github.com/OmniaGit/odooplm.git
