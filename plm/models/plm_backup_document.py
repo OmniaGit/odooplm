6b5830849b8d40590212fdb3ac7b441647207c0e
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-12-16 18:20:59 +0100

    fix: missing

--- plm/models/plm_backup_document.py
+++ plm/models/plm_backup_document.py
@@ -138,7 +138,7 @@ class PlmBackupDocument(models.Model):
         """
         import glob
         file_store = self.env['ir.attachment']._filestore()
-        for path in glob.glob(f"{file_store}/**/*",recursive=True):
+        for path in glob.glob(r"%s}/**/*" % file_store,recursive=True):
             if not os.path.isdir(path):
                 if not self.env['ir.attachment'].search_count([("store_fname",'ilike',os.path.basename(path))]):
                     try:
