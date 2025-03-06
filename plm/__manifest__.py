2365b93062532c07c3232e5d3607a50f4d8574a9
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2025-03-06 11:10:10 +0100

    just the revision index of the module

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "16.0.29",
+    "version": "16.0.33",
     "author": "OmniaSolutions",
     "website": "https://odooplm.omniasolutions.website",
     "category": "Manufacturing/Product Lifecycle Management (PLM)",
