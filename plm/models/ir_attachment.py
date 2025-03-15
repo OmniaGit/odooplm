c7b1c31448205f0f3f81b5d784d86ea80c9036f9
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2025-03-15 07:25:16 +0100

    FIX: problem on error
    once on saving error occurs the system is not able to update the
    has_error field due to the not triggered function on write_date

--- plm/models/ir_attachment.py
+++ plm/models/ir_attachment.py
@@ -1303,7 +1303,7 @@ class IrAttachment(models.Model):
     #
     #
     #
-    @api.depends("write_date")
+    @api.depends("datas")
     def _checkSavingError(self):
         for ir_attachment_id in self:
             ir_attachment_id.has_error = not ir_attachment_id.is_last_save_ok()
@@ -1675,6 +1675,26 @@ class IrAttachment(models.Model):
     def GetNextDocumentName(self, documentName):
         """
             Return a new name due to sequence next number.
+            
+            NEW client set in the contex this informations
+            {
+            #
+            #
+            #
+            'document_attrs': {} # dictionary like of attribute changed for products
+            'product_attrs': {} # dictionary like of attribute changed for documents
+            'ori_document_attrs': {} # dictionary like of attribute from cad application for products
+            'orig_product_attrs': {} # dictionary like of attribute from cad application for documents
+            'mode':'ir.attachment' or 'product.product' 
+            #
+            # those other value are for compatibylity with the old version
+            #
+            'active_file_doc_attrs': {}, 
+            'all_attributes': {'product': self.attributes,
+                                'document':self._documentAttrs
+            },
+            #
+            'mode':'ir.attachment'
         """
         ctx = self.env.context
         eng_code = ctx.get('product_attrs', {}).get("engineering_code") or ctx.get('engineering_code', '')
