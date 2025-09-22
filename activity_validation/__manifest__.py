930c1814adab0f70f5ca83aacb0035e2492bb807
Kuldip Trapasiya <kuldip.trapasiya@aktivsoftware.com>
2025-09-22 19:03:49 +0530

    [ADD]plm: Migrated plm_pdf_workorder, activity_validation modules from version 18 to version 19.

--- activity_validation/__manifest__.py
+++ activity_validation/__manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     "name": "Activity Validation",
-    "version": "18.0.1.0.5",
+    "version": "19.0.1.0.0",
     "author": "OmniaSolutions",
     "website": "https://github.com/OmniaGit/odooplm",
     "category": "Custom",
--- plm_pdf_workorder/__manifest__.py
+++ plm_pdf_workorder/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     "name": "PLM Report PDF Workorder",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "OmniaSolutions",
     "website": "https://odooplm.omniasolutions.website",
     "category": "Manufacturing/Product Lifecycle Management (PLM)",
