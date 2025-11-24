e0bca6aad6377e3e6a702fc5a084541861c4b35c
Kuldip <kuldip.trapasiya@aktivsoftware.com>
2025-11-24 16:57:13 +0530

    [ADD]plm: Added a new module, plm_purchase_share, which provides functionality to download product documents from the purchase order on the portal.

---
+++ plm_purchase_share/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OmniaSolutions, Open Source Management Solution
+#    Copyright (C) 2010-2021 OmniaSolutions (<https://www.omniasolutions.website>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "PLM Purchase Share",
+    "version": "18.0.1.0.0",
+    "author": "OmniaSolutions",
+    "website": "https://odooplm.omniasolutions.website",
+    "sequence": 15,
+    "license": "AGPL-3",
+    "summary": "download the products document from the purchase orders on the portal.",
+    "description": """
+        Allow to download documents from the portal.
+        """,
+    "depends": ["plm", "purchase"],
+    "data": [
+        # security
+        "security/ir.model.access.csv",
+
+        # views
+        "views/portal_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ plm_purchase_share/controllers/main.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+import io
+import zipfile
+
+
+class PortalPurchaseDownload(http.Controller):
+
+    @http.route('/my/purchase/<int:order_id>/download_docs', type='http', auth='public', website=True)
+    def download_purchase_documents(self, order_id, **kwargs):
+        order = request.env['purchase.order'].sudo().browse(order_id)
+        if not order or not order.exists():
+            return request.not_found()
+
+        zip_buffer = io.BytesIO()
+        zip_file = zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED)
+
+        report_action = request.env.ref(
+            'plm.report_product_product_pdf_latest'
+        ).sudo()
+
+        Report = request.env['ir.actions.report'].sudo()
+
+        for line in order.order_line:
+            product = line.product_id
+            if not product:
+                continue
+
+            html_content, _ = report_action._render_qweb_html("plm.report_product_product_pdf_latest", product.id)
+
+            wrapped_html = f"""
+                            <html>
+                                <main>
+                                    {html_content}
+                                </main>
+                            </html>
+                        """
+
+            bodies = Report._prepare_html(wrapped_html)[0]
+
+            pdf_content = Report._run_wkhtmltopdf(bodies)
+
+            filename = f"{product.display_name.replace('/', '_')}.pdf"
+            zip_file.writestr(filename, pdf_content)
+
+        zip_file.close()
+        zip_buffer.seek(0)
+
+        zip_filename = f"PO_{order.name}_Documents.zip"
+        return request.make_response(
+            zip_buffer.getvalue(),
+            headers=[
+                ('Content-Type', 'application/zip'),
+                ('Content-Disposition', f'attachment; filename=\"{zip_filename}\"'),
+            ]
+        )
