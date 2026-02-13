0c6c0aa86a7848ebc2528330fd38d725ef75a274
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2026-02-13 10:54:15 +0100

    imp:

--- plm/models/ir_attachment.py
+++ plm/models/ir_attachment.py
@@ -4089,8 +4089,7 @@ class IrAttachment(models.Model):
         self.ensure_one()
         #
         if latest:
-            out_main_id = self.get_latest_version().id
-            for document_id in out_main_id.getDocBomFlat(latest):
+            for document_id in self.getDocBomFlat(latest):
                 out.append(document_id.get_download_dict(hostname,
                                                          hostpws))
         else:
