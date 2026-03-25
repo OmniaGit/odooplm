ddb3632afd6a72d4180d7b557d917e0031da90a5
jayraj-omnia <jayraj.thakkar@aktivsoftware.com>
2026-03-25 13:03:27 +0530

    [ADD] | PLM: Open Docuemnt with Revision

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "18.0.20.0.1",
+    "version": "18.0.20.0.2",
     "author": "OmniaSolutions",
     "website": "https://odooplm.omniasolutions.website",
     "category": "Manufacturing/Product Lifecycle Management (PLM)",
