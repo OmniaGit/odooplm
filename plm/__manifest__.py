d0a2a8d48f9e76428d5cd7f3b3fdcae20c7d7a14
jayraj-omnia <jayraj.thakkar@omniasolutions.eu>
2026-08-11 16:21:42 +0530

    [FIX] : Document access issue for non plm user

--- plm/__manifest__.py
+++ plm/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     "name": "Product Lifecycle Management",
-    "version": "19.0.1.0.14",
+    "version": "19.0.1.0.15",
     "author": "OmniaSolutions",
     "maintainer": "OmniaSolutions S.n.c di Boscolo Matteo & C",
     "website": "https://odooplm.omniasolutions.website",
