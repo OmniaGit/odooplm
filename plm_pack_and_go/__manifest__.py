f867de713cb7d67f131e1ea66a684100edb0ba08
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2026-09-23 23:21:27 +0200

    [MOD] | Bump the versions of the modules changed since origin/19.0

--- plm_pack_and_go/__manifest__.py
+++ plm_pack_and_go/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     "name": "Plm Pack and Go",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.0.2",
     "images": ["static/description/cover.gif"],
     "author": "OmniaSolutions",
     "maintainer": "OmniaSolutions S.n.c di Boscolo Matteo & C",
